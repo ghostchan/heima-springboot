f273c3a61d3e1b9669e7b3304337f9b6edf00cbd
ghostchan <jinnchan@qq.com>
2021-11-08 22:08:46 +0800

    05-多个yml文件配置

--- src/main/java/com/itheima/Controller/HelloController.java
+++ src/main/java/com/itheima/Controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.itheima.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +13,16 @@ public class HelloController {
     @Autowired
     private DataSource dataSource;
 
+    @Value("${itcast.url}")
+    private String itcastUrl;
+
+    @Value("${itheima.url}")
+    private String itheimaUrl;
+
     @GetMapping("hello")
     public String hello() {
+        System.out.println(" itcastUrl = " + itcastUrl);
+        System.out.println(" itheimaUrl " + itheimaUrl);
         System.out.println("DataSource" + dataSource);
         return "Hello,Spring Boot!";
     }
