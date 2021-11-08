3770063a143d6d68a7e27d11c816fe5b060b3c86
ghostchan <jinnchan@qq.com>
2021-11-08 21:31:49 +0800

    04-Spring Boot属性注入方式

--- src/main/java/com/itheima/config/JdbcConfig.java
+++ src/main/java/com/itheima/config/JdbcConfig.java
@@ -2,18 +2,18 @@ package com.itheima.config;
 
 
 import com.alibaba.druid.pool.DruidDataSource;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 
 @Configuration
-@PropertySource("classpath:jdbc.properties")
+//@EnableConfigurationProperties(JdbcProperties.class)
 public class JdbcConfig {
 
-    @Value("${jdbc.url}")
+  /*  @Value("${jdbc.url}")
     String url;
 
     @Value("${jdbc.driverClassName}")
@@ -33,6 +33,23 @@ public class JdbcConfig {
         dataSource.setUsername(username);
         dataSource.setPassword(password);
         return dataSource;
-    }
+    }*/
 
+
+/*    @Bean
+    public DataSource dataSource(JdbcProperties jdbcProperties) {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName(jdbcProperties.getDriverClassName());
+        dataSource.setUrl(jdbcProperties.getUrl());
+        dataSource.setUsername(jdbcProperties.getUsername());
+        dataSource.setPassword(jdbcProperties.getPassword());
+        return dataSource;
+    }*/
+
+
+    @Bean
+    @ConfigurationProperties(prefix = "jdbc")
+    public DataSource dataSource() {
+        return new DruidDataSource();
+    }
 }
