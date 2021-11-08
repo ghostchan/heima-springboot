e01977c701c70e0110d482606b053b468d306ebb
ghostchan <jinnchan@qq.com>
2021-11-08 21:00:36 +0800

    03-Java代码方式配置

--- src/main/java/com/itheima/Controller/HelloController.java
+++ src/main/java/com/itheima/Controller/HelloController.java
@@ -1,13 +1,20 @@
 package com.itheima.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    private DataSource dataSource;
+
     @GetMapping("hello")
     public String hello() {
+        System.out.println("DataSource" + dataSource);
         return "Hello,Spring Boot!";
     }
 }
