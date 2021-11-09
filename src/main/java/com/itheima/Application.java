7d34d56cfbcfd69d05960ce0b7d1c82b61cdaf60
ghostchan <jinnchan@qq.com>
2021-11-09 22:21:08 +0800

    11-Spring Boot整合-Mybatis

--- src/main/java/com/itheima/Application.java
+++ src/main/java/com/itheima/Application.java
@@ -1,9 +1,12 @@
 package com.itheima;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//扫描mybatis所有的业务mapper接口
+@MapperScan("com.itheima.mapper")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
